7bb75b22029fb041bd57faa2a6e4b6bf24c3e6f1
mukta <muktalgrg@gmail.com>
2017-07-26 14:33:18 +0545

    adding load_album_list method for the first time

--- first_server.js
+++ first_server.js
@@ -1,12 +1,36 @@
 const http = require('http');
+const fs = require('fs');
+
+function load_album_list(callback){
+    fs.readdir('albums', (err, files)=>{
+        if(err){
+            callback(err);
+        }else{
+            callback(null, files);
+        }
+    });
+}
 
 function process_request(req,res){
     console.log("server is running at port 8080");
+
+    load_album_list((err, albums)=>{
+        if(err){
+            res.writeHead(500, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify({code: 'cant_load_albums', message: err.message}));
+        }else{
+            let output = {error: null, data: {albums: albums}};
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify(output)+'\n');
+        }
+    });
+
     console.log("METHOD : "+req.method+"\nReqeust URL : "+req.url);
     
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({error: null, data:{}}));
+    // res.writeHead(200, {'Content-Type': 'application/json'});
+    // res.end(JSON.stringify({error: null, data:''}));
     
 }
 
 const s = http.createServer(process_request).listen(8080);
+
