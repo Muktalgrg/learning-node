2856436e94bb419e20d3465f450861a3a714e83e
mukta <muktalgrg@gmail.com>
2017-07-28 14:33:04 +0545

    fixing url

--- first_server.js
+++ first_server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 
 
 function load_album_list(callback){
@@ -21,13 +22,12 @@ function load_album_list(callback){
                 });
             }
             iterator(0);
-
         }
     });
 }
 
 // for example inside albums find that one of australia--> albums/australia
-function load_album(album_name, callback){
+function load_album(album_name, page, page_size, callback){
     fs.readdir('albums/'+album_name,(err, files)=>{
         if(err){
             //console.log(err);
@@ -42,7 +42,8 @@ function load_album(album_name, callback){
 
             let iterator = (index) => {
                 if(index === files.length){
-                    console.log(only_files);
+                    const start = page * page_size;
+                    const output = only_files.slice(start, start + page_size);
                     let obj = { short_name: album_name.substr(1),
                                  photos: only_files};
                     callback(null, obj);
@@ -60,14 +61,18 @@ function load_album(album_name, callback){
     });
 }
 
+// main function -->
 function handle_incoming_request(req,res){
-    console.log("INCOMING REQUEST : "+req.method+"\nURL"+req.url);
+    // adding property
+    req.parsed_url = url.parse(req.url, true); // true changes query string into json obj
+    const core_url = req.parsed_url.pathname; // everything infront of ?
+    console.log("INCOMING REQUEST : "+req.method+"\nURL"+core_url);
 
-    if(req.url === '/albums.json'){
+    if( req.url === '/albums.json'){
         handle_load_album_list(req, res);
         
-    }else if(req.url.substr(0,7) == '/albums'
-            && req.url.substr(req.url.length - 5) == '.json'){
+    }else if(core_url.substr(0,7) == '/albums'
+            && core_url.substr(core_url.length - 5) == '.json'){
         handle_load_album(req, res);
          
     }else{
@@ -115,7 +120,8 @@ function handle_load_album_list(req, res){
 }
 
 function handle_load_album(req, res){
-    load_album(req.url.substr(7,(req.url.length - 12)/* /albums/australia.json whole - (.json+/albums) */ ),(err, photos)=>{
+    const core_url = req.parsed_url.pathname;
+    load_album(core_url.substr(7,(core_url.length - 12)/* /albums/australia.json whole - (.json+/albums) */ ),(err, photos)=>{
                 if(err){
                     send_failure(res, 500, err);
                     
